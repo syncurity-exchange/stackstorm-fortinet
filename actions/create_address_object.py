72e11d879b2daf196f651b879acc0c188e392984
JP Bourget <jp@syncurity.net>
2019-10-18 23:43:13 -0400

    add repr to create address object

--- actions/create_address_object.py
+++ actions/create_address_object.py
@@ -3,7 +3,7 @@ from lib.action import FortinetBaseAction
 
 class CreateAddressObject(FortinetBaseAction):
     def run(self, name=None, payload=None):
-        status = self.device.create_firewall_address(name, payload)
+        status = self.device.create_firewall_address(name, repr(payload))
         if status == 200:
             return (True, status)
         return (False, status)

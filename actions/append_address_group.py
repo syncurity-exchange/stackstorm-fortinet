b216882348320a7d985e2f9cf83fdef545806b48
JP Bourget <jp@syncurity.net>
2019-10-22 16:21:54 -0400

    added append address group action

---
+++ actions/append_address_group.py
@@ -0,0 +1,14 @@
+from lib.action import FortinetBaseAction
+
+
+class UpdateAddressGroup(FortinetBaseAction):
+    def run(self, name=None, member=None):
+
+        group_members = self.device.get_address_group(name=name)
+        new_member = {'name': member, 'q_origin_key': member}
+        group_members[0][member].append(new_member)
+        payload = "{\'member\': " + str(group_members[0]['member']) + "}"
+        status_code = self.device.update_address_group(name, payload)
+        if status_code == 200:
+            return True, status_code
+        return False, status_code

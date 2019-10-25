cbba7dfff5e02a5868200cba713d88efdcdcc0f9
JP Bourget <jp@syncurity.net>
2019-10-25 13:22:33 -0400

    update get_address group for fields

--- actions/get_address_object.py
+++ actions/get_address_object.py
@@ -2,8 +2,8 @@ from lib.action import FortinetBaseAction
 
 
 class GetAddressObject(FortinetBaseAction):
-    def run(self, name=None):
-        addresses = self.device.get_firewall_address(name)
+    def run(self, name=None, filter=None):
+        addresses = self.device.get_firewall_address(name, filter=filter)
         if isinstance(addresses, list):
             return True, addresses
         return False, addresses

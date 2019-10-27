ab55e403533720b3800e147b7fff2de2984eac3b
JP Bourget <jp@syncurity.net>
2019-10-26 23:33:02 -0400

    get single address object

--- actions/get_address_group.py
+++ actions/get_address_group.py
@@ -2,8 +2,8 @@ from lib.action import FortinetBaseAction
 
 
 class GetAddressGroup(FortinetBaseAction):
-    def run(self, name=None, output_format=None):
-        addresses = self.device.get_address_group(name)
+    def run(self, name=None, output_format=''):
+        addresses = self.device.get_address_group(name, output_format=output_format)
         if isinstance(addresses, list):
             return True, addresses
         return False, addresses
